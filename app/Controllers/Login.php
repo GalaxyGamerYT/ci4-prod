88998c68087fda31ea6820103e12e6308e25abbc
GalaxyGamerYT <OliWootton@Outlook.com>
2024-01-08 17:01:18 +0000

    fixed pressing login submit button when fields are empty

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -23,54 +23,52 @@ class Login extends BaseController
     public function authenticate()
     {
         $session = session();
-        $session->set('log','False');
-        $rules = [
-            'username' => ['rules' => 'required|min_length[4]|max_length[255]|is_unique[users.username]'],
-            'email' => ['rules' => 'required|min_length[4]|max_length[255]|valid_email|is_unique[users.email]'],
-            'password' => ['rules' => 'required|min_length[8]|max_length[255]'],
-        ];
+        $usersModel = new UsersModel();
+        $preferencesModel = new PreferencesModel();
+        $privilegesModel = new PrivilegesModel();
 
-        if($this->validate($rules)){
-            $session->set('log','True');
-            $usersModel = new UsersModel();
-            $preferencesModel = new PreferencesModel;
-            $privilegesModel = new PrivilegesModel;
+        $username_input = $this->request->getVar('username');
+        $email = $this->request->getVar('email');
+        $password = $this->request->getVar('password');
 
-            $email = $this->request->getVar('email');
-            $password = $this->request->getVar('password');
-
-            $user = $usersModel->where('email', $email)->first();
-            $preferences = $preferencesModel->where('user_id', $user['user_id'])->first();
-            $privileges = $privilegesModel->where('user_id', $user['user_id'])->first();
-
-            if(is_null($user)) {
-                return redirect()->back()->withInput()->with('error', 'Invalid username or password.');
-            }
-
-            $pwd_verify = password_verify($password, $user['password']);
-
-            if(!$pwd_verify) {
-                return redirect()->back()->withInput()->with('error', 'Invalid username or password.');
-            }
-
-            $ses_data = [
-                'user_id' => $user['user_id'],
-                'username' => $user['username'],
-                'email' => $user['email'],
-                'isLoggedIn' => TRUE,
-                'privilege_id' => $privileges['privilege_id'],
-                'admin' => $privileges['admin'],
-                'preference_id' => $preferences['preference_id'],
-            ];
-
-            $session->set($ses_data);
-            return redirect()->to('/dashboard');
-        }else{
-            $data['validation'] = $this->validator;
-            return view('login', $data);
+        if(is_null($username_input) or $username_input == ""){
+            return redirect()->back()->withInput()->with('error', 'Please enter a username.');
         }
 
-        
+        if(is_null($email) or $email == ""){
+            return redirect()->back()->withInput()->with('error', 'Please enter an email.');
+        }
+
+        if(is_null($password) or $password == ""){
+            return redirect()->back()->withInput()->with('error', 'Please enter a password.');
+        }
+
+        $user = $usersModel->where('email', $email)->first();
+        $preferences = $preferencesModel->where('user_id', $user['user_id'])->first();
+        $privileges = $privilegesModel->where('user_id', $user['user_id'])->first();
+
+        if(is_null($user)) {
+            return redirect()->back()->withInput()->with('error', 'Invalid username.');
+        }
+
+        $pwd_verify = password_verify($password, $user['password']);
+
+        if(!$pwd_verify) {
+            return redirect()->back()->withInput()->with('error', 'Invalid password.');
+        }
+
+        $ses_data = [
+            'user_id' => $user['user_id'],
+            'username' => $user['username'],
+            'email' => $user['email'],
+            'isLoggedIn' => TRUE,
+            'privilege_id' => $privileges['privilege_id'],
+            'admin' => $privileges['admin'],
+            'preference_id' => $preferences['preference_id'],
+        ];
+
+        $session->set($ses_data);
+        return redirect()->to('/dashboard');
     }
 
     public function logout() {
